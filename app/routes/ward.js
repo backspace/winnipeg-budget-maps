808191d4411fe39fa9ceec41824d404f6515488f
Buck Doyle <b@chromatin.ca>
2020-01-18 00:29:08 -0600

    Add councillor contact links

--- app/routes/ward.js
+++ app/routes/ward.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
 import Wards from '../data/wards';
 import Photos from '../data/councillor-photos';
+import Contacts from '../data/councillor-contacts';
 
 export default class WardRoute extends Route {
   model({ name }) {
@@ -8,9 +9,18 @@ export default class WardRoute extends Route {
     const councillorName = ward.properties.councillor;
     const photoUrl = Photos.find(person => person.name === councillorName).photo;
 
+    const contact = Contacts.find(c => c.person === councillorName);
+    const emailLink = contact.email_link;
+    const recipientIndex = emailLink.indexOf('Recipient=');
+    const ampersandAfterRecipientIndex = emailLink.indexOf('&', recipientIndex + 1);
+    const emailUsername = emailLink.substring(recipientIndex + 10, ampersandAfterRecipientIndex);
+    const email = `${emailUsername}@winnipeg.ca`;
+
     return {
       ward,
-      photoUrl
+      photoUrl,
+      phone: contact.phone,
+      email,
     }
   }
 }
