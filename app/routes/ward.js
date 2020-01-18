6eb543a87c11444ee25d0e7a8445cef5e14d4442
Buck Doyle <b@chromatin.ca>
2020-01-18 01:46:47 -0600

    Add closure list for facilities in ward
    This could be done with preprocessing but it seems quick
    enough on the phone I use, at least… some facilities are
    close to the border of other wards, hmm

--- app/routes/ward.js
+++ app/routes/ward.js
@@ -1,13 +1,19 @@
 import Route from '@ember/routing/route';
-import Wards from '../data/wards';
 import Photos from '../data/councillor-photos';
 import Contacts from '../data/councillor-contacts';
 
 import resolveAsset from 'ember-cli-resolve-asset';
+import insidePolygon from 'point-in-polygon';
 
 export default class WardRoute extends Route {
   async model({ name }) {
-    const ward = Wards.features.find(ward => ward.properties.name === name);
+    const {
+      libraries,
+      pools,
+      wards,
+    } = this.modelFor('application');
+
+    const ward = wards.features.find(ward => ward.properties.name === name);
     const councillorName = ward.properties.councillor;
     const photoFilename = Photos.find(person => person.name === councillorName).photo;
     const photoUrl = await resolveAsset(`councillors/${photoFilename}`);
@@ -19,11 +25,16 @@ export default class WardRoute extends Route {
     const emailUsername = emailLink.substring(recipientIndex + 10, ampersandAfterRecipientIndex);
     const email = `${emailUsername}@winnipeg.ca`;
 
+    const closures = libraries.concat(pools)
+      .filter(facility => facility.closure)
+      .filter(facility => insidePolygon([facility.lon, facility.lat], ward.geometry.coordinates[0][0]));
+
     return {
       ward,
       photoUrl,
       phone: contact.phone,
       email,
+      closures,
     }
   }
 }
