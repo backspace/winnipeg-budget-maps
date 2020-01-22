da51c5b3c85139989ff18ca1b545ebf6f56eb906
Buck Doyle <b@chromatin.ca>
2020-01-21 23:04:33 -0600

    Add missed closure for Eldon Ross pool

--- app/data/closures.js
+++ app/data/closures.js
@@ -6,6 +6,7 @@ export default {
   'Happyland Pool': new Date(2021, 5, 1),
   'Windsor Park Pool': new Date(2021, 5, 1),
 
+  'Eldon Ross Pool': new Date(2020, 8, 1),
   'Kinsmen Sherbrook Pool': new Date(2022, 0, 1),
   'Transcona Kinsmen Centennial Pool': new Date(2022, 0, 1),
 };
