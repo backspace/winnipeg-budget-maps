9362f06f910a5289d237999fa92be6a43aafec88
Buck Doyle <b@chromatin.ca>
2020-01-24 10:52:25 -0600

    Extract preprocessing row extraction function

--- scripts/preprocess-data.js
+++ scripts/preprocess-data.js
@@ -15,55 +15,12 @@ fs.writeFileSync('app/data/wards.json', JSON.stringify(Wards, null, 2));
 
 const inside = require('point-in-geopolygon');
 
-const Libraries = require('../data/libraries.json');
+const processedLibraries = extractRows(require('../data/libraries.json').data, 'library', 19);
+const processedIndoorPools = extractRows(require('../data/indoor-pools.json').data, 'indoor-pool', 45);
+const processedOutdoorPools = extractRows(require('../data/outdoor-pools.json').data, 'outdoor-pool', 37);
+const processedWadingPools = extractRows(require('../data/wading-pools.json').data, 'wading-pool', 28);
+const processedArenas = extractRows(require('../data/arenas.json').data, 'arena', 15);
 
-const processedLibraries = Libraries.data.map(row => {
-  const name = row[8], lat = parseFloat(row[19][1]), lon = parseFloat(row[19][2]);
-  return {
-    name,
-    lat,
-    lon,
-    type: 'library',
-    ward: Wards.features.find(ward => inside.polygon(ward.geometry.coordinates[0], [lon, lat])).properties.name,
-  };
-});
-
-const IndoorPools = require('../data/indoor-pools.json');
-const OutdoorPools = require('../data/outdoor-pools.json');
-const WadingPools = require('../data/wading-pools.json');
-
-const processedIndoorPools = IndoorPools.data.map(row => {
-  const name = row[8], lat = parseFloat(row[45][1]), lon = parseFloat(row[45][2]);
-  return {
-    name,
-    lat,
-    lon,
-    type: 'indoor-pool',
-    ward: Wards.features.find(ward => inside.polygon(ward.geometry.coordinates[0], [lon, lat])).properties.name,
-  }
-});
-
-const processedOutdoorPools = OutdoorPools.data.map(row => {
-  const name = row[8], lat = parseFloat(row[37][1]), lon = parseFloat(row[37][2]);
-  return {
-    name,
-    lat,
-    lon,
-    type: 'outdoor-pool',
-    ward: Wards.features.find(ward => inside.polygon(ward.geometry.coordinates[0], [lon, lat])).properties.name,
-  }
-});
-
-const processedWadingPools = WadingPools.data.map(row => {
-  const name = row[8], lat = parseFloat(row[28][1]), lon = parseFloat(row[28][2]);
-  return {
-    name,
-    lat,
-    lon,
-    type: 'wading-pool',
-    ward: Wards.features.find(ward => inside.polygon(ward.geometry.coordinates[0], [lon, lat])).properties.name,
-  }
-});
 
 const closedWadingPools = [];
 const remainingWadingPools = [...processedWadingPools];
@@ -96,18 +53,6 @@ closedWadingPools.forEach((pool, index) => {
   pool.syntheticClosureDateString = `${closureYear}-07-01 23:00:00`;
 });
 
-const Arenas = require('../data/arenas.json');
-
-const processedArenas = Arenas.data.map(row => {
-  const name = row[8], lat = parseFloat(row[15][1]), lon = parseFloat(row[15][2]);
-  return {
-    name,
-    lat,
-    lon,
-    type: 'arena',
-    ward: Wards.features.find(ward => inside.polygon(ward.geometry.coordinates[0], [lon, lat])).properties.name,
-  }
-});
 
 fs.writeFileSync('app/data/facilities.json', JSON.stringify(processedLibraries.concat(processedIndoorPools, processedOutdoorPools, processedWadingPools, processedArenas), null, 2));
 
@@ -117,3 +62,15 @@ function calculateDistance(a, b) {
     Math.pow(b.lon - a.lon, 2)
   )
 }
+
+function extractRows(rows, type, positionColumn) {
+  return rows.map(row => {
+    const name = row[8], lat = parseFloat(row[positionColumn][1]), lon = parseFloat(row[positionColumn][2]);
+    return {
+      name,
+      lat,
+      lon,
+      type,
+      ward: Wards.features.find(ward => inside.polygon(ward.geometry.coordinates[0], [lon, lat])).properties.name,
+    };
+  });}
