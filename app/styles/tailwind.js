f0ed71dd94f3032e5a0b5b4c22afc456738182aa
Buck Doyle <b@chromatin.ca>
2020-01-24 17:42:15 -0600

    Change colours to be extended vs replaced
    Without this, the layers button had no background colour.

--- app/styles/tailwind.js
+++ app/styles/tailwind.js
@@ -1,14 +1,15 @@
 module.exports = {
   theme: {
-    colors: {
-      blu: '#2E368F',
-      gold: '#F9AE3A',
+    extend: {
+      colors: {
+        blu: '#2E368F',
+        gold: '#F9AE3A',
+      },
     },
     fontFamily: {
       'body': ['body'],
       'headline': ['headline'],
     },
-    extend: {}
   },
   variants: {},
   plugins: []
