ed84e5118ceb16f1c558fa7b738879bebba92917
Buck Doyle <b@chromatin.ca>
2020-01-25 18:21:03 -0600

    Add autoprefixing for CSS
    The clip path for the bus animation wasn’t working on iOS,
    and a prefix makes it work… better to use this, though.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -22,6 +22,7 @@ module.exports = function(defaults) {
     postcssOptions: {
       compile: {
         plugins: [
+          require('autoprefixer'),
           {
             module: require('postcss-nested'),
             options: {
