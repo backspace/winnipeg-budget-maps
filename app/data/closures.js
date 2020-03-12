4f9988852d7003477fca4ff8dcc9c8641a51a90d
Buck Doyle <b@chromatin.ca>
2020-03-11 18:40:20 -0700

    Remove arena, pool, and library closures

--- app/data/closures.js
+++ app/data/closures.js
@@ -1,18 +1,3 @@
 export default {
-  'Charles A. Barbour Arena': new Date(2022, 0, 1),
-  'Bertrand Arena': new Date(2022, 0, 1),
-  'Maginot Arena': new Date(2022, 0, 1),
-  'Sargent Park Arena': new Date(2021, 0, 1),
   'Terry Sawchuk Arena': new Date(2019, 7, 30),
-
-  'Fort Garry Library': new Date(2022, 0, 1),
-  'West Kildonan Library': new Date(2020, 8, 1),
-  'Westwood Library': new Date(2020, 8, 1),
-
-  'Happyland Pool': new Date(2021, 5, 1),
-  'Windsor Park Pool': new Date(2021, 5, 1),
-
-  'Eldon Ross Pool': new Date(2020, 8, 1),
-  'Kinsmen Sherbrook Pool': new Date(2022, 0, 1),
-  'Transcona Kinsmen Centennial Pool': new Date(2022, 0, 1),
 };
